1463bda50c61288bb8d5b658eb7c64d192af6e0e
thing-zoo <cathy4025@gmail.com>
2023-07-15 10:40:32 +0900

    feat: init commit

---
+++ src/main/java/com/example/moviewbackend/MoviewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviewbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviewBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviewBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moviewbackend/MoviewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviewbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviewBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

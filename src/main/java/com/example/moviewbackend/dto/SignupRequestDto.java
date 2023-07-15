34884fc5fbe71893d3be9ec3d72f3c92f8388dca
bbororo5 <114351464+bbororo5@users.noreply.github.com>
2023-07-15 16:04:55 +0900

    7/15 오후 4시 일단 제출

---
+++ src/main/java/com/example/moviewbackend/dto/SignupRequestDto.java
@@ -0,0 +1,22 @@
+package com.example.moviewbackend.dto;
+
+import com.example.moviewbackend.entity.UserRoleEnum;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
+@Getter
+public class SignupRequestDto {
+    @Pattern(regexp = "^[a-z0-9]{4,10}$",
+            message = "최소 4자 이상, 10자 이하이며 알파벳 소문자(a~z), 숫자(0~9) 로 구성되어야 합니다.")
+    private String nickname;
+
+    @Email
+    private String email;
+
+    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()_+{}:\"<>?,.\\\\/]{8,15}$",
+            message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자 로 구성되어야 합니다.")
+    private String password;
+
+    private UserRoleEnum role; // 회원 권한 (ADMIN, USER)
+}

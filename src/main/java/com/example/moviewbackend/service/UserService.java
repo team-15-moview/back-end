34bdc7d5ef42c2a3851bca75c2734b9142ce567e
bbororo5 <114351464+bbororo5@users.noreply.github.com>
2023-07-17 10:40:32 +0900

    [Fix] 로그인 및 회원가입 수정

--- src/main/java/com/example/moviewbackend/service/UserService.java
+++ src/main/java/com/example/moviewbackend/service/UserService.java
@@ -5,8 +5,6 @@ import com.example.moviewbackend.entity.User;
 import com.example.moviewbackend.entity.UserRoleEnum;
 import com.example.moviewbackend.jwt.JwtUtil;
 import com.example.moviewbackend.repository.UserRepository;
-import io.jsonwebtoken.Claims;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;

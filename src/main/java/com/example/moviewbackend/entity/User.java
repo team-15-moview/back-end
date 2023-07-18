53200a71e58eab4a84046ed07a5808810dc072f6
thing-zoo <cathy4025@gmail.com>
2023-07-18 14:39:19 +0900

    fix: kakaoId로 인한 entity 에러 해결

--- src/main/java/com/example/moviewbackend/entity/User.java
+++ src/main/java/com/example/moviewbackend/entity/User.java
@@ -3,8 +3,6 @@ package com.example.moviewbackend.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +10,6 @@ import java.util.List;
 @Getter
 @Table(name = "users")
 @NoArgsConstructor
-
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,37 +24,22 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = true)
+    @Column
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
 
-
-    private Long kakaoId;
-  
     @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
     private List<Review> reviews = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
     private List<Like> likes = new ArrayList<>();
 
-    public User(String nickname, String password, String email, UserRoleEnum role, Long kakaoId) {
+    public User(String nickname, String password, String email, UserRoleEnum role) {
         this.nickname = nickname;
         this.password = password;
         this.email = email;
         this.role = role;
-        this.kakaoId =kakaoId;
     }
 
-    public User(String email, String password, String nickname, UserRoleEnum role) {
-        this.password = password;
-        this.email = email;
-        this.nickname = nickname;
-        this.role = role;
-    }
-
-    public User kakaoIdUpdate(Long kakaoId) {
-        this.kakaoId = kakaoId;
-        return this;
-    }
 }
 
